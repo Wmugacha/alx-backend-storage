e2f57a3e7b3febd3c5b7b97d80c7a7e26097313d
Wmugacha <wilfredmugacha@gmail.com>
2023-08-12 17:09:39 +0000

    avg score procedure

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,17 +1,14 @@
 -- SQL script that creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0
 DELIMITER //
-
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 2)
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 4)
 BEGIN
-    DECLARE result DECIMAL(10, 2);
-    
+    DECLARE result DECIMAL(10, 4);
     IF b = 0 THEN
-        RETURN 0;
+        SET result = 0;
     ELSE
         SET result = a / b;
-        RETURN result;
     END IF;
+    RETURN result;
 END;
 //
-
 DELIMITER ;
---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,31 @@
+-- SQL script that creates a stored procedure that computes and store the average weighted score for a student.
+DELIMITER //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_score DECIMAL(10, 2);
+    DECLARE total_weight DECIMAL(10, 2);
+    DECLARE average_weighted_score DECIMAL(10, 2);
+
+    -- Initialize variables
+    SET total_score = 0;
+    SET total_weight = 0;
+
+    -- Calculate the total weighted score and total weight
+    SELECT SUM(score * weight), SUM(weight) INTO total_score, total_weight
+    FROM corrections
+    JOIN projects ON corrections.project_id = projects.id
+    WHERE corrections.user_id = user_id;
+
+    -- Calculate the average weighted score
+    IF total_weight > 0 THEN
+        SET average_weighted_score = total_score / total_weight;
+    ELSE
+        SET average_weighted_score = 0;
+    END IF;
+
+    -- Update the average weighted score in the users table
+    UPDATE users SET average_score = average_weighted_score WHERE id = user_id;
+
+END;
+//
+DELIMITER ;
