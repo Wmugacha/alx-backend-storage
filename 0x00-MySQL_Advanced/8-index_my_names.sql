a692fc9c368edd0f70b32ead3b909c07cb50eb63
Wmugacha <wilfredmugacha@gmail.com>
2023-08-12 15:46:28 +0000

    firstname index

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,8 @@
 -- SQL script that creates an index idx_name_first on the table names and the first letter of name
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
 
+-- Add a computed column for the first letter
+ALTER TABLE names
+ADD COLUMN first_letter CHAR(1) AS (LEFT(name, 1)) STORED;
+
+-- Create an index on the computed column
+CREATE INDEX idx_name_first ON names (first_letter);
